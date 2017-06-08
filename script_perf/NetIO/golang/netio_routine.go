82dcb5fef117ad88d1898e70043cc8bf745887cf
dapanfei <623746467@qq.com>
2017-06-08 19:13:24 +0800

    edit go code and  add some annotations

--- script_perf/NetIO/golang/netio_routine.go
+++ script_perf/NetIO/golang/netio_routine.go
@@ -9,14 +9,15 @@ import (
 	"time"
 )
 
-var response = "HTTP/1.0 200 OK\r\nCache-Control: no-cache\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n<html><body><h1>200 OK</h1>\nHello world.\n</body></html>\n"
+var response = []byte("HTTP/1.0 200 OK\r\nCache-Control: no-cache\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n<html><body><h1>200 OK</h1>\nHello world.\n</body></html>\n")
 var rps = 0
 
 func main() {
 	port := flag.String("port", "8081", "Specify the port")
 	cores := flag.Uint("cores", 1, "Specify the CPU cores using")
 	flag.Parse()
-
+	
+	/*go 1.8 after,No settings required runtime.GOMAXPROCS */
 	runtime.GOMAXPROCS(int(*cores))
 
 	addr := "0.0.0.0:" + *port
@@ -43,16 +44,17 @@ func main() {
 
 func worker(conn net.Conn) {
 	defer conn.Close()
-
-	buf := make([]byte, 1024)
-	bytes, err := conn.Read(buf)
+	var buf [1024]byte
+	
+	/* Maybe a little better than  buf := make([]byte,1024) ,but it's not the best way to deal with it */
+	bytes, err := conn.Read(buf[0:])
 	if err != nil {
 		return
 	}
 
 	if bytes > 0 {
 		rps++
-		_, err = conn.Write([]byte(response))
+		_, err = conn.Write(response)
 	}
 }
 
